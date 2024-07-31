236ea44c3854b206640596bbc10599f59dc43986
Praveenadapa425 <147489706+Praveenadapa425@users.noreply.github.com>
2024-07-31 15:37:35 +0530

    import java.util.*; public class ap {     public static void main(String[] args)     {         int r;         Scanner read=new Scanner(System.in);         r=read.nextInt();         System.out.printf("%.2f ",3.14*r*r);         System.out.printf("%.2f",2*3.14*r);
        }
    }

---
+++ Area_and_Perimeter_of_Circle.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class ap
+{
+    public static void main(String[] args)
+    {
+        int r;
+        Scanner read=new Scanner(System.in);
+        r=read.nextInt();
+        System.out.printf("%.2f
+",3.14*r*r);
+        System.out.printf("%.2f",2*3.14*r);
+        
+    }
+}
